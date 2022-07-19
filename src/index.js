5451b736c26fd8061ad81ceb45c0144e68304526
Pedro Pereira <plp.leoncio@gmail.com>
2022-07-19 18:48:30 -0300

    imagens + base da tela inicial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import ReactDom from "react-dom";
 
 function App () {
     return (
-        <div className="content">teste</div>
+        <div className="content">
+            <img src="./assets/img/logo.png" alt=""></img>
+            <span className="tittle">ZapRecall</span>
+            <div className="button">Iniciar Recall!</div>
+        </div>
     )
 }
 ReactDom.render(<App />, document.querySelector(".root"));
