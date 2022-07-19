81b8ee28695b0d3c5b7d3dcc7a06ef49139b0637
Pedro Pereira <plp.leoncio@gmail.com>
2022-07-19 18:23:23 -0300

    criação das pastas e arquivos base

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDom from "react-dom";
+
+function App () {
+    return (
+        <div className="content">teste</div>
+    )
+}
+ReactDom.render(<App />, document.querySelector(".root"));
